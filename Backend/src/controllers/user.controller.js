8e64dd5ff9579502428463d624338b7f011e518a
sahilraj87083 <sahilraj87093@gmail.com>
2026-01-09 14:28:46 +0530

    feat: implement logout endpoint with refresh token invalidation

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -92,7 +92,34 @@ const loginUser = asyncHandler(async(req, res) => {
 })
 
 const logoutUser = asyncHandler(async(req, res) => {
+    const userId = req.user._id;
+    
+    if (!userId) {
+        throw new ApiError(401, "Unauthorized request")
+    }
 
+    const user = await User.findByIdAndUpdate(
+        userId, 
+        {
+            $unset : {
+                refreshToken : 1
+            }
+        },
+        {
+            new : true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie('refreshToken', options)
+    .json(new ApiResponse(200, "User logged Out", {}))
 })
 
 
