5f47ab1b9826c3cae34e58db58cfb69014e25db0
sahilraj87083 <sahilraj87093@gmail.com>
2026-01-09 14:22:05 +0530

    feat: implement user login with JWT access and refresh tokens

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -46,7 +46,49 @@ const registerUser = asyncHandler(async(req, res) => {
 })
 
 const loginUser = asyncHandler(async(req, res) => {
+    const {email, password} = req.body
 
+    if(!email){
+        throw new ApiError(400, "Email is required")
+    }
+
+    const user = await User.findOne({
+        email : email
+    })
+
+    if(!user){
+         throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await user.verifyPassword(password)
+
+    if(!isPasswordValid){
+         throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const {accessToken, refreshToken} = generateAccessAndRefereshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+
+    const option = {
+        httpOnly : true,
+        secure : true
+    }
+
+    return res
+    .status(200)
+    .cookie('accessToken', accessToken, option)
+    .cookie('refreshToken', refreshToken, option)
+    .json(
+        new ApiResponse(
+            200,
+            "User logged In Successfully",
+            {
+                user : loggedInUser
+            }
+        )
+    )
 })
 
 const logoutUser = asyncHandler(async(req, res) => {
