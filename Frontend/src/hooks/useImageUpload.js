29b858c26bc6a4d83b4ca1a891887f4b5e81c582
sahilraj87083 <sahilraj87093@gmail.com>
2026-01-29 06:07:56 +0530

    refactor(hooks): improve image upload hook safety and memory handling

---
+++ Frontend/src/hooks/useImageUpload.js
@@ -0,0 +1,64 @@
+import { useState, useEffect} from "react";
+import toast from "react-hot-toast";
+import { updateCoverService, updateAvatarService } from "../services/auth.services";
+
+const serviceMap = {
+    avatar : updateAvatarService,
+    coverImage : updateCoverService
+}
+
+export const useImageUpload = (type, onSuccess) => {
+    const [file, setFile] = useState(null)
+    const [preview, setPreview] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        return () => {
+            if (preview) URL.revokeObjectURL(preview);
+        };
+    }, [preview]);
+
+
+    const selectFile = (file) => {
+        setFile(file)
+        setPreview(URL.createObjectURL(file));
+    }
+
+    const upload = async () => {
+        if(!file){
+            toast.error("Please select an image");
+            return;
+        }
+
+        const formData = new FormData()
+        formData.append(type, file)
+        try {
+            
+            if (!serviceMap[type]) {
+                throw new Error(`Invalid upload type: ${type}`);
+            }
+
+            setLoading(true)
+            
+            const user = await serviceMap[type](formData)
+            toast.success(`${type} updated successfully`);
+            onSuccess?.(user);
+            setFile(null)
+            return user;
+        } catch (error) {
+            toast.error(`Failed to update ${type}`);
+            throw error;
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
+    return {
+        file,
+        preview,
+        loading,
+        selectFile,
+        upload,
+    };
+}
