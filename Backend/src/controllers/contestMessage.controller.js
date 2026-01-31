f008f4a5bfffc8404fe81864b72e93608ba6e899
sahilraj87083 <sahilraj87093@gmail.com>
2026-02-01 04:47:16 +0530

    services updated to include user's fullName and avatar

--- Backend/src/controllers/contestMessage.controller.js
+++ Backend/src/controllers/contestMessage.controller.js
@@ -1,14 +1,13 @@
 import { asyncHandler } from "../utils/AsyncHandler.utils.js"
-import { getContestMessagesService , createContestMessageService, createSystemMessageService} from '../services/contestMessage.service.js'
+import { getContestMessagesService } from '../services/contestMessage.service.js'
 import { paginate } from "../utils/pagination.utils.js"
+import { ApiResponse } from "../utils/ApiResponse.utils.js"
 
 
 const getContestChatMessages = asyncHandler( async (req, res) => {
-
     const { contestId } = req.params;
     const {p , l} = req.query
     const {skip , page , limit} = paginate({p , l});
-    const room = getPrivateRoom(req.user._id, userId);
 
     const messages = await getContestMessagesService({
         contestId,
--- Backend/src/services/contestMessage.service.js
+++ Backend/src/services/contestMessage.service.js
@@ -1,13 +1,20 @@
 import { ContestMessage } from '../models/contestMessage.model.js'
 
 export const createContestMessageService = async ({contestId, senderId, message, messageType = "text", phase = "lobby"}) => {
-    return ContestMessage.create({
+    const msg = await ContestMessage.create({
         contestId : contestId,
         senderId : senderId,
         message : message,
         messageType : messageType,
         phase : phase
     })
+
+    const populated = await msg.populate({
+        path: "senderId",
+        select: "fullName avatar username",
+    });
+
+    return populated;
 }
 
 export const createSystemMessageService = async ({contestId , message, phase = "live"}) => {
@@ -21,7 +28,7 @@ export const createSystemMessageService = async ({contestId , message, phase = "
 }
 
 export const getContestMessagesService = async ({ contestId, page, skip, limit }) => {
-    return ContestMessage.find({ contestId })
+    return ContestMessage.find({ contestId }).populate("senderId", "fullName avatar")
         .sort({ createdAt: -1 })
         .skip(skip)
         .limit(limit)
