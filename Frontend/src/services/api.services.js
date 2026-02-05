f7fe7721ff4f738e9b62ebcb93f0697ebe3ac797
sahilraj87083 <sahilraj87093@gmail.com>
2026-02-05 05:38:18 +0530

    feat : production bug fixed

--- Frontend/src/services/api.services.js
+++ Frontend/src/services/api.services.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 // 1. Determine the URL based on the environment
 const baseURL = import.meta.env.PROD 
-  ? import.meta.env.VITE_PROD_API  // Uses Render link when deployed
-  : import.meta.env.VITE_LOCAL_API; // Uses Localhost when running locally
+  ? (import.meta.env.VITE_PROD_API || "https://reviclash.onrender.com")  // Uses Render link when deployed
+  : (import.meta.env.VITE_LOCAL_API || "http://localhost:4000"); // Uses Localhost when running locally
 
 export const api = axios.create({
   baseURL: `${baseURL}/api/v1`, 
