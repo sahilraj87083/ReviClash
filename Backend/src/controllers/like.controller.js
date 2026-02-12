38ba09387ac8d08f8cb9a8709674f282c4992bff
sahilraj87083 <sahilraj87093@gmail.com>
2026-02-13 03:36:29 +0530

    ft : post like endpoint controller added

--- Backend/src/controllers/like.controller.js
+++ Backend/src/controllers/like.controller.js
@@ -3,13 +3,113 @@ import { ApiResponse } from "../utils/ApiResponse.utils";
 import { ApiError } from "../utils/ApiError.utils";
 import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/cloudinary.utils.js";
 import { Like } from "../models/like.model.js";
+import { isValidObjectId } from "mongoose";
+import { Post } from "../models/post.model.js";
 
 const togglePostLike = asyncHandler( async(req, res) => {
+    const { postId } = req.params
 
+    if(!postId || isValidObjectId(postId)){
+        throw new ApiError(403, "Invalid post Id");
+    }
+
+    try {
+        await Like.create({ userId: req.user._id, postId });
+        await Post.updateOne({ _id: postId }, { $inc: { likeCount: 1 } });
+        return res.json(new ApiResponse(200, "Post liked", null));
+    } catch (err) {
+
+        if (err.code !== 11000) throw err;
+
+        await Like.deleteOne({ userId: req.user._id, postId });
+        await Post.updateOne({ _id: postId }, { $inc: { likeCount: -1 } });
+        return res.json(new ApiResponse(200,"Post unliked", null));
+    }
 })
 
 
 const getAllLikedPost = asyncHandler( async(req, res) => {
+    const {cursor , limit = 5} = req.query
+
+    const safeLimit = limit > 0 ? limit : 10;
+
+    const query = {
+        userId : new mongoose.Types.ObjectId(req.user._id)
+    }
+
+    if(cursor){
+        query.cursor = { $lt : new Date(cursor)}
+    }
+
+    const likedPosts = await Post.aggregate([
+        {
+            $match : query
+        },
+
+        { $sort: { createdAt: -1 } },
+        { $limit: Number(safeLimit) + 1 },
+
+        // join post
+        {
+            $lookup : {
+                from : 'posts',
+                localField : 'postId',
+                foreignField : '_id',
+                as: "post"
+            }
+        },
+        { $unwind : '$post'},
+
+        // join user
+        {
+            $lookup : {
+                from : 'users',
+                localField : 'post.authorId',
+                foreignField : '_id',
+                as : 'author'
+            }
+        },
+        { $unwind : '$author'},
+
+        {
+            $project: {
+                _id: 0,
+                likedAt: "$createdAt",
+                post: {
+                    _id: "$post._id",
+                    textContent: "$post.textContent",
+                    images: "$post.images",
+                    videos: "$post.videos",
+                    likeCount: "$post.likeCount",
+                    commentCount: "$post.commentCount",
+                    repostCount: "$post.repostCount",
+                    createdAt: "$post.createdAt"
+                },
+                author: {
+                    _id: "$author._id",
+                    username: "$author.username",
+                    avatar: "$author.avatar.url"
+                }
+            }
+        }
+    ])
+
+    let nextCursor = null
+    const hasMore = false
+    if(likedPosts.length > safeLimit){
+        const nextItem = likedPosts.pop();
+        nextCursor = nextItem.createdAt
+        hasMore = true
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, {
+            posts: likedPosts,
+            nextCursor,
+            hasMore
+        })
+    );
+
 
 })
 
