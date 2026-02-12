a473bc01d2cfa21835850d3bc6c6b09acb2ed589
sahilraj87083 <sahilraj87093@gmail.com>
2026-02-13 04:48:40 +0530

    ft: improved following feed to support reosted posts

--- Backend/src/controllers/feed.controller.js
+++ Backend/src/controllers/feed.controller.js
@@ -1,8 +1,8 @@
 import { asyncHandler } from "../utils/AsyncHandler.utils";
 import { ApiResponse } from "../utils/ApiResponse.utils";
-import { ApiError } from "../utils/ApiError.utils";
 import { Post } from "../models/post.model.js";
 import { Follow } from "../models/follow.model.js";
+import { Repost } from "../models/repost.model.js";
 
 const getGeneralfeed = asyncHandler( async(req, res) => {
     const { cursor, limit = 15 } = req.query
@@ -49,35 +49,68 @@ const getFriendsFeed = asyncHandler( async(req, res) => {
 
     const ids = following.map((f) => (f.followingId))
 
-    const query = {
+    const postQuery = {
         authorId : {
             $in : ids
         }
     }
+    const repostQuery = {
+        userId: { $in: ids }
+    };
 
     if(cursor){
-        query.createdAt = { $lt : new Date(cursor)}
+        postQuery.createdAt = { $lt: new Date(cursor) };
+        repostQuery.createdAt = { $lt: new Date(cursor) };
     }
 
-    const posts = await Post.find(query)
-                .sort({ createdAt : -1})
-                .limit(safeLimit + 1)
-                .populate("authorId", "username fullname avatar.url")
-                .lean();
+    const [posts , reposts] = Promise.all([
+        Post.find(postQuery)
+            .populate("authorId", "username fullname avatar.url")
+            .lean(),
+
+        Repost.find(repostQuery)
+            .populate({
+                path: "postId",
+                populate: {
+                    path: "authorId",
+                    select: "username fullname avatar.url"
+                }
+            })
+            .populate("userId", "username fullname avatar.url")
+            .lean()
+    ])
+
+    // Normalize
+    const normalizedPosts = posts.map(post => ({
+        type: "post",
+        activityTime: post.createdAt,
+        data: post
+    }));
+    
+    const normalizedReposts = reposts.map(r => ({
+        type: "repost",
+        activityTime: r.createdAt,
+        repostedBy: r.userId,
+        data: r.postId
+    }));
+
+    const combined = [...normalizedPosts, ...normalizedReposts]
+        .sort((a, b) => new Date(b.activityTime) - new Date(a.activityTime))
+        .slice(0, safeLimit + 1);
 
     let nextCursor = null
     const hasMore = false
 
-    if(posts.length > safeLimit){
-        const nextItem = posts.pop();
-        nextCursor = nextItem.createdAt
+    if(combined.length > safeLimit){
+        const nextItem = combined.pop();
+        nextCursor = nextItem.activityTime;
         hasMore = true
     }
 
     return res.status(200).json(
-        new ApiResponse(200, "Posts fetched Successfully",
+        new ApiResponse(200, "Feed fetched Successfully",
             {
-                posts: posts,
+                posts: combined,
                 nextCursor,
                 hasMore
             }
