cccab368f72fb13e50a689ce0b42153235191ba3
sahilraj87083 <sahilraj87093@gmail.com>
2026-02-22 02:47:14 +0530

    ft : comment model upgraded to support comment replies

--- Backend/src/models/comment.model.js
+++ Backend/src/models/comment.model.js
@@ -18,10 +18,26 @@ const commentSchema = new mongoose.Schema({
         ref : 'User',
         required: true,
         index: true
+    },
+    // To support nested replies
+    parentId: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
+        default: null,
+        index: true
+    },
+    
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    replyCount: {
+        type: Number,
+        default: 0
     }
-},{timestamps : true})
+}, { timestamps : true })
 
-commentSchema.index({ postId : 1, createdAt: -1 })
+// Optimized index to fetch top-level comments for a post quickly
+commentSchema.index({ postId : 1, parentId: 1, createdAt: -1 });
 
-
-export const Comment = mongoose.model('Comment',commentSchema)
+export const Comment = mongoose.model('Comment', commentSchema);
